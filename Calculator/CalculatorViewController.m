ba1266928e94484cb0359cc989122c1062e57a6e
Jaime Machuca <jmachuca77@mac.com>
2012-07-03 12:51:34 +0200

    Added Decimal Point Operations

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -12,6 +12,7 @@
 
 @interface CalculatorViewController ()
 @property (nonatomic) BOOL userIsInTheMiddleOfEnteringANumber;
+@property (nonatomic) BOOL thisIsTheFirstDecimalPoint;
 @property (nonatomic, strong) CalculatorBrain *brain;
 @end
 
@@ -20,6 +21,7 @@
 @synthesize display = _display;
 @synthesize brain = _brain;
 @synthesize userIsInTheMiddleOfEnteringANumber;
+@synthesize thisIsTheFirstDecimalPoint;
 
 - (CalculatorBrain *) brain
 {
@@ -46,7 +48,7 @@
 
 - (IBAction)digitPressed:(UIButton *)sender {
     NSString *digit = [sender currentTitle];
-    if(userIsInTheMiddleOfEnteringANumber){
+    if(self.userIsInTheMiddleOfEnteringANumber){
         self.display.text = [self.display.text stringByAppendingString:digit];        
     } else {
         self.display.text = digit;
@@ -55,11 +57,37 @@
 
     
 }
+- (IBAction)decimalPointPressed:(UIButton *)sender {
+
+    NSString *digit = [sender currentTitle];
+    if (userIsInTheMiddleOfEnteringANumber) {
+        if (!thisIsTheFirstDecimalPoint) {
+            self.display.text = [self.display.text stringByAppendingString:digit];
+            self.thisIsTheFirstDecimalPoint = YES;
+        } else {
+            /* Send Alert View */
+ 			UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Invalid Floating Point" delegate:nil cancelButtonTitle:@"Close" otherButtonTitles:nil];
+			[alertView show];
+        }
+    } else {
+        self.display.text = digit;
+        self.userIsInTheMiddleOfEnteringANumber = YES;
+        self.thisIsTheFirstDecimalPoint = YES;
+    }
+}
 
 - (IBAction)enterPressed
 {
-    [self.brain pushOperand:[self.display.text doubleValue]];
-    self.userIsInTheMiddleOfEnteringANumber = NO;
+    if([self.display.text isEqualToString:@"π"])
+        {
+        [self.brain pushOperand:3.14159];
+        }
+    else
+        {
+        [self.brain pushOperand:[self.display.text doubleValue]];
+        }
+        self.userIsInTheMiddleOfEnteringANumber = NO;
+    self.thisIsTheFirstDecimalPoint = NO;
 }
 
 - (IBAction)operationPressed:(UIButton *)sender
